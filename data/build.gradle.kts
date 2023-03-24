40b92fd718c61083a6f851db7287d98b12d39186
Mira <miraidajaanbaeva@gmail.com>
2023-03-24 18:57:13 +0600

    Add data module

---
+++ data/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id(Plugins.AGP.library)
+    id(Plugins.Kotlin.android)
+    id(Plugins.Kotlin.kapt)
+    id(Plugins.DaggerHilt.hilt)
+}
+
+android {
+    namespace = "com.example.lecture_1a_7"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    //DI Hilt
+    implementation(Dependencies.DaggerHilt.android)
+    kapt(Dependencies.DaggerHilt.compiler)
+
+    //Coroutine
+    implementation(Dependencies.Coroutine.core)
+
+    //Room
+    implementation(Dependencies.Room.room)
+    implementation(Dependencies.Room.runtime)
+    kapt(Dependencies.Room.compiler)
+}
