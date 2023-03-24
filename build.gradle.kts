d839618e1e06d00f93858587983e0b90af4b60f1
Mira <miraidajaanbaeva@gmail.com>
2023-03-24 18:29:02 +0600

    Transform top build.gradle to .kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id 'com.android.application' version '7.3.1' apply false
-    id 'com.android.library' version '7.3.1' apply false
-    id 'org.jetbrains.kotlin.android' version '1.7.20' apply false
+    id(Plugins.AGP.application) version Versions.AGP apply false
+    id(Plugins.AGP.library) version Versions.AGP apply false
+    id(Plugins.Kotlin.android) version Versions.kotlin apply false
+    id(Plugins.DaggerHilt.hilt) version Versions.daggerHilt apply false
+    id(Plugins.Kotlin.jvm) version Versions.kotlin apply false
 }
